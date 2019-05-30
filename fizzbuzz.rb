a25b9defcc6f6bda6789ccd8b18e87caddfe1028
tommaso auerbach <tsa302@nyu.edu>
2019-05-30 21:36:08 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,22 @@
-# Don't forget! This file needs to be 'required' in its spec file
+require_relative '../fizzbuzz.rb'
+require_relative './spec_helper.rb'
+
+def fizzbuzz
+
+end
+
+describe fizzbuzz do
+  def fizzbuzz(int)
+    if int % 3 == 0
+      "Fizz"
+    end
+  end
+
+describe fizzbuzz do
+  def fizzbuzz(int)
+    if int % 5 == 0
+      "Buzz"
+    end
+  end
+  # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
